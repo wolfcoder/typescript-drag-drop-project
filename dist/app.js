81c710e6cecd66f2dd1484b5370acdb7e529bc3f
bams <bbg2021@gmail.com>
2020-10-30 04:45:13 +0800

    122. Dom Element Selection & OOP Rendering

--- dist/app.js
+++ dist/app.js
@@ -1,4 +1,15 @@
 "use strict";
-// Code goes here!
-const x = 1;
+class ProjectInput {
+    constructor() {
+        this.templateElement = document.getElementById('project-input');
+        this.hostElement = document.getElementById('app');
+        const importedNode = document.importNode(this.templateElement.content, true);
+        this.element = importedNode.firstElementChild;
+        this.attach();
+    }
+    attach() {
+        this.hostElement.insertAdjacentElement('afterbegin', this.element);
+    }
+}
+const prjInput = new ProjectInput();
 //# sourceMappingURL=app.js.map
